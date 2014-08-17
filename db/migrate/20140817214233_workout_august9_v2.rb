ff621110bbb78a82385f8cb93779358201acc540
Stephen Karger <stephen.karger@gmail.com>
2014-08-17 18:24:46 -0400

    fix type error in migration

--- db/migrate/20140817214233_workout_august9_v2.rb
+++ db/migrate/20140817214233_workout_august9_v2.rb
@@ -6,19 +6,23 @@ class WorkoutAugust9V2 < ActiveRecord::Migration
         w.type = 'SpeedWorkout'
         w.save
 
-        w.run_intervals.create({ :order => 1, :distance_in_meters => 200,
+        sw = SpeedWorkout.find(w.id)
+
+        sw.run_intervals.create({ :order => 1, :distance_in_meters => 200,
           :time => '34 seconds', :rest => '2 minutes' })
-        w.run_intervals.create({ :order => 2, :distance_in_meters => 400,
+        sw.run_intervals.create({ :order => 2, :distance_in_meters => 400,
           :time => '70 seconds', :rest => '3 minutes' })
-        w.run_intervals.create({ :order => 3, :distance_in_meters => 200,
+        sw.run_intervals.create({ :order => 3, :distance_in_meters => 200,
           :time => '34 seconds', :rest => '2 minutes 30 seconds' })
-        w.run_intervals.create({ :order => 4, :distance_in_meters => 400,
+        sw.run_intervals.create({ :order => 4, :distance_in_meters => 400,
           :time => '72 seconds'})
       end
 
       m.down do
         w = Workout.where(when: DateTime.new(2014, 8, 9, 16, 0, 0, '-4')).first
         w.run_intervals.destroy_all
+        w.type = nil
+        w.save
       end
     end
   end
