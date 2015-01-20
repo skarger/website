b91ecfc3e495b1409ed24269292b3b65aa3939f5
Stephen Karger <stephen.karger@gmail.com>
2015-01-19 22:05:52 -0500

    add some user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should not allow a User with a blank email address' do
+    u = User.new()
+    expect(u.valid?).to be_falsey
+  end
+
+  it 'should accept a User that has an email address' do
+    u = User.new(email: 'test@test.com', password: 'secret')
+    expect(u.valid?).to be_truthy
+  end
+
+  it 'should ensure that email address is unique' do
+    u = User.new(email: 'test@test.com', password: 'secret')
+    u.save
+    v = u.dup
+    expect(v.valid?).to be_falsey
+  end
 end
