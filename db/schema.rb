f541bd6dd25e656f159bd59fde211b66307a58b2
Stephen Karger <stephen.karger@gmail.com>
2015-01-19 22:13:09 -0500

    migration to add unique email index on users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105005802) do
+ActiveRecord::Schema.define(version: 20150120031040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,11 +34,13 @@ ActiveRecord::Schema.define(version: 20141105005802) do
     t.string   "name"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+
   create_table "workouts", force: true do |t|
     t.datetime "when"
     t.string   "where"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "notes"
     t.string   "type"
   end
