2e7c6432ad86ae557d6e4214cc4e4c46fe195c3b
Stephen Karger <stephen.karger@gmail.com>
2015-02-19 21:55:38 -0500

    create new run interval form

---
+++ spec/views/run_intervals/new.html.erb_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "run_intervals/new", :type => :view do
+
+  before(:each) do
+    workout = SpeedWorkout.new(id: 1)
+    run_interval = RunInterval.new(speed_workout_id: workout.id)
+    assign(:workout, workout)
+    assign(:run_interval, run_interval)
+  end
+
+  it 'should have an input for the order of the interval' do
+    render
+    expect(rendered).to match(/Order/)
+  end
+
+  it 'should have an input box for distance in meters' do
+    render
+    expect(rendered).to match(/Distance in meters/)
+  end
+
+  it 'should have an input for the time taken' do
+    render
+    expect(rendered).to match(/Time/)
+  end
+
+  it 'should have an input for the rest' do
+    render
+    expect(rendered).to match(/Rest/)
+  end
+
+end
