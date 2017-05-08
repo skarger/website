86bb59b8a64285eaf5b9d76fbc00051122f7d4f2
Stephen Karger <stephen.karger@gmail.com>
2017-05-08 13:57:14 -0400

    re-install webpacker elm with latest PR revisions

--- config/webpack/loaders/elm.js
+++ config/webpack/loaders/elm.js
@@ -1,5 +1,18 @@
+const path = require('path')
+const { env } = require('../configuration.js')
+
+const elmSource = path.resolve(process.cwd())
+
+const loaderOptions = () => {
+  if (['development', 'test'].includes(env.NODE_ENV)) {
+    return `elm-hot-loader!elm-webpack-loader?cwd=${elmSource}&verbose=true&warn=true&debug=true`
+  }
+
+  return `elm-webpack-loader?cwd=${elmSource}`
+}
+
 module.exports = {
   test: /\.elm$/,
   exclude: [/elm-stuff/, /node_modules/],
-  loader: 'elm-hot-loader!elm-webpack-loader?verbose=true&warn=true&debug=true'
+  loader: loaderOptions()
 }
