168f58f83847462c58c30cea95b6868962812982
Stephen Karger <stephen.karger@gmail.com>
2017-05-07 22:02:53 -0400

    migration to create locations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,49 +10,58 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113170013) do
+ActiveRecord::Schema.define(version: 20170508014546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "postgis"
 
-  create_table "distance_runs", force: :cascade do |t|
-    t.decimal  "distance_in_miles"
-    t.string   "time"
-    t.integer  "distance_workout_id"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.index ["distance_workout_id"], name: "index_distance_runs_on_distance_workout_id", using: :btree
+  create_table "distance_runs", id: :serial, force: :cascade do |t|
+    t.decimal "distance_in_miles"
+    t.string "time"
+    t.integer "distance_workout_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["distance_workout_id"], name: "index_distance_runs_on_distance_workout_id"
   end
 
-  create_table "track_intervals", force: :cascade do |t|
-    t.integer  "order"
-    t.integer  "distance_in_meters"
-    t.string   "time"
-    t.string   "rest"
+  create_table "locations", force: :cascade do |t|
+    t.text "name"
+    t.geography "point", limit: {:srid=>4326, :type=>"st_point", :geographic=>true}
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["point"], name: "index_locations_on_point"
+  end
+
+  create_table "track_intervals", id: :serial, force: :cascade do |t|
+    t.integer "order"
+    t.integer "distance_in_meters"
+    t.interval "time"
+    t.interval "rest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "track_workout_id"
+    t.integer "track_workout_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password_digest"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
-    t.string   "remember_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.string "name"
+    t.string "remember_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  create_table "workouts", force: :cascade do |t|
+  create_table "workouts", id: :serial, force: :cascade do |t|
     t.datetime "when"
-    t.string   "name"
+    t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "notes"
-    t.string   "type"
-    t.integer  "user_id"
-    t.index ["user_id"], name: "index_workouts_on_user_id", using: :btree
+    t.string "notes"
+    t.string "type"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_workouts_on_user_id"
   end
 
   add_foreign_key "distance_runs", "workouts", column: "distance_workout_id", on_delete: :cascade
