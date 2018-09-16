dfba7fdba2fc2884bb267f6adbe0898f233b1a11
Stephen Karger <stephen.karger@gmail.com>
2018-09-15 20:25:56 -0400

    Remove timeout initializer

--- config/initializers/timeout.rb
+++
@@ -1,5 +0,0 @@
-# config/initializers/timeout.rb
-# originally copied from https://devcenter.heroku.com/articles/rails-unicorn
-if Rails.env.production?
-  Rack::Timeout.timeout = 10  # seconds
-end
