8a672af00357934a4b5984a42f57185d021f2b4c
Stephen Karger <stephen.karger@gmail.com>
2019-12-30 22:54:59 -0500

    Fix tests

--- tests/about_test.rs
+++ tests/about_test.rs
@@ -1,14 +1,14 @@
-use actix_web::dev::Service;
 use actix_web::{test, App};
 use web_server;
 
-#[test]
-fn about_get() {
+#[actix_rt::test]
+async fn about_get() {
     let mut app = test::init_service(
         App::new()
-            .configure(web_server::config));
+            .configure(web_server::config))
+            .await;
     let req = test::TestRequest::get().uri("/about").to_request();
-    let resp = test::block_on(app.call(req)).unwrap();
+    let resp = test::call_service(&mut app, req).await;
 
     assert!(resp.status().is_success());
 }
--- tests/static_files_test.rs
+++ tests/static_files_test.rs
@@ -1,23 +1,23 @@
-use actix_web::dev::Service;
 use actix_web::{test, App, http::StatusCode};
 use web_server;
 
-#[test]
-fn static_get() {
+#[actix_rt::test]
+async fn static_get() {
     let mut app = test::init_service(
         App::new()
-            .configure(web_server::config));
+            .configure(web_server::config))
+            .await;
 
     let req = test::TestRequest::get().uri("/static/404.html").to_request();
-    let resp = test::block_on(app.call(req)).unwrap();
+    let resp = test::call_service(&mut app,req).await;
     assert!(resp.status().is_success(), "Successfully serves static 404 page");
 
     let req = test::TestRequest::get().uri("/static/favicon.ico").to_request();
-    let resp = test::block_on(app.call(req)).unwrap();
+    let resp = test::call_service(&mut app,req).await;
     assert!(resp.status().is_success(), "Successfully serves favicon");
 
     let req = test::TestRequest::get().uri("/static/unknown.jpg").to_request();
-    let resp = test::block_on(app.call(req)).unwrap();
+    let resp = test::call_service(&mut app,req).await;
     assert!(resp.status().is_client_error(), "Returns 400-level error for unknown static file");
     assert_eq!(resp.status(), StatusCode::NOT_FOUND, "Returns 404 for unknown static file");
 }
