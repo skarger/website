2531806bd0ca202678036db2aaa82eb44e55e964
Stephen Karger <stephen.karger@gmail.com>
2020-01-03 15:16:03 -0500

    Add style.css

--- src/web/index.js
+++ src/web/index.js
@@ -1,5 +1,9 @@
+import './style.css';
+
 function component() {
   const element = document.createElement('div');
+  element.classList.add('hello');
+  element.innerHTML = 'Hello webpack';
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/web/index.js',
-  mode: 'development',
+  entry: "./src/web/index.js",
+  mode: "development",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'static/dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "static/dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+        ],
+      },
+    ],
   },
 };
