cf35833fe30be3f1a3f57edebe552f193564d151
Stephen Karger <stephen.karger@gmail.com>
2020-01-03 16:19:15 -0500

    Drop unused JS

--- src/web/index.js
+++ src/web/index.js
@@ -1,11 +1,1 @@
 import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-  element.classList.add('hello');
-  element.innerHTML = 'Hello webpack';
-
-  return element;
-}
-
-document.body.appendChild(component());
